003891e636b7a102b441c7171ad75978ca3f4fd7
tarisadinda20 <tarisadinda20@gmail.com>
2023-10-26 21:05:44 +0700

    set up scss

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    prependData: `@import "@/styles/helpers/_variables.scss";
+    @import "@/styles/helpers/_button.scss";
+    `,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  }
 }
 
 module.exports = nextConfig
