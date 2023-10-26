5a7715c44f8198fa5667f50767bf264445669bf0
tarisadinda20 <tarisadinda20@gmail.com>
2023-10-27 00:19:44 +0700

    add redux & theme

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,25 @@
-import '@/styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import '@/styles/globals.scss'
+import { ThemeProvider, createTheme } from '@mui/material'
+import { Provider } from 'react-redux'
+import { store } from '@/redux/store'
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Inter, sans-serif',
+  },
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const getLayout = Component.getLayout || ((page) => page)
+
+  return getLayout(<>
+    <ThemeProvider theme={theme}>
+      <StyledEngineProvider injectFirst>
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      </StyledEngineProvider>
+    </ThemeProvider>
+  </>) 
 }
