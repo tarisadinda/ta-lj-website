3ccceffbf9daa016a5797ea0178f831490745f37
tarisadinda20 <tarisadinda20@gmail.com>
2023-10-28 16:10:16 +0700

    add font style

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import '@/styles/globals.scss'
-import { ThemeProvider, createTheme } from '@mui/material'
+import { StyledEngineProvider, ThemeProvider, createTheme } from '@mui/material'
 import { Provider } from 'react-redux'
-import { store } from '@/redux/store'
+import { store } from 'src/redux/store'
+import { Inter } from 'next/font/google'
 
-const theme = createTheme({
-  typography: {
-    fontFamily: 'Inter, sans-serif',
-  },
+const inter = Inter({
+  weight: ['400', '600', '700', '800'],
+  subsets: ['latin'],
 })
 
 export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page)
+
+  const theme = createTheme({
+    typography: {
+      fontFamily: inter,
+    },
+  })
 
   return getLayout(<>
     <ThemeProvider theme={theme}>
