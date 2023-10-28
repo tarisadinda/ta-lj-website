b61a7367347141edb4688feb83fa7f022c7804cb
tarisadinda20 <tarisadinda20@gmail.com>
2023-10-28 14:23:29 +0700

    add date convert function

---
+++ src/utils/convert-date.js
@@ -0,0 +1,11 @@
+export const convertDate = (timestamp) => {
+    const date = new Date(timestamp);
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const seconds = date.getSeconds().toString().padStart(2, '0');
+    
+    return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
+}
