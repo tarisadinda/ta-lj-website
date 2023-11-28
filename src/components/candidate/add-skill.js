d0b9d572e864886700977417c19c62a77861f80f
afif14 <mafifsolahudin@gmail.com>
2023-11-28 11:51:50 +0700

    fixing edit profile, filter home candidate

--- src/components/candidate/add-skill.js
+++ src/components/candidate/add-skill.js
@@ -4,23 +4,30 @@ import CustomDropdown from "../common/dropdown";
 import { axiosInstance } from "src/utils/axios";
 import { CustomChip } from "../common/chip";
 import { useDispatch, useSelector } from "react-redux";
-import { alertMessage, openAlert, severity, setMessage, setOpenAlert, setSeverity } from "src/redux/common/alertSlice";
+import {
+  alertMessage,
+  openAlert,
+  severity,
+  setMessage,
+  setOpenAlert,
+  setSeverity,
+} from "src/redux/common/alertSlice";
 import CustomAlert from "../common/alert";
 
-const AddSkill = ({ open, onClose}) => {
+const AddSkill = ({ open, onClose }) => {
   const [pagination, setPagination] = useState({
     size: 10,
     page: 0,
     search: "",
   });
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const [skillOptions, setSkillOptions] = useState([]);
   const [selectedSkills, setSelectedSkills] = useState([]);
   const [selectedOptions, setSelectedOptions] = useState([]);
 
-  const isOpenAlert = useSelector(openAlert)
-  const alertMsg = useSelector(alertMessage)
-  const alertSeverity = useSelector(severity)
+  const isOpenAlert = useSelector(openAlert);
+  const alertMsg = useSelector(alertMessage);
+  const alertSeverity = useSelector(severity);
 
   const getAllSkills = () => {
     axiosInstance
@@ -47,8 +54,8 @@ const AddSkill = ({ open, onClose}) => {
   }, []);
 
   const handleSelectedSkillsChange = (event) => {
-    const selectedValues = event.target.value
-      
+    const selectedValues = event.target.value;
+
     const newSelectedOptions = selectedValues
       .map((value) => {
         const selectedOption = skillOptions.find(
@@ -65,24 +72,36 @@ const AddSkill = ({ open, onClose}) => {
     setSelectedSkills(selectedValues);
   };
 
-  // Menghapus skill yang dipilih dari daftar selectedSkills
-  const handleRemoveSkill = (skillToRemove) => {
-    const updatedSkills = selectedSkills.filter(
-      (skill) => skill.value !== skillToRemove.value
+  const handleRemoveSkill = (indexToRemove) => {
+    const updatedSkills = [...selectedSkills]; // Duplikat array agar tidak mengubah array asli
+    updatedSkills.splice(indexToRemove, 1); // Menghapus elemen pada indeks yang diberikan
+
+    const updatedOptions = [...selectedOptions]; // Duplikat array agar tidak mengubah array asli
+    const filteredOptions = updatedOptions.filter(
+      (item) => item.value !== indexToRemove + 1
     );
+
     setSelectedSkills(updatedSkills);
+    setSelectedOptions(filteredOptions);
   };
 
   const handleSaveSkill = () => {
-    axiosInstance.post('/candidateDetail/addSkill', {skill: selectedSkills.toString()}).then((res) => {
-      if(res) {
-        dispatch(setOpenAlert(true))
-        dispatch(setMessage('Profile berhasil diperbarui!'))
-        dispatch(setSeverity('success'))
-        setTimeout(onClose, 2000)
-      }
-    }).catch((err) => console.log(err))
-  }
+    axiosInstance
+      .post("/candidateDetail/addSkill", { skill: selectedSkills })
+      .then((res) => {
+        if (res) {
+          dispatch(setOpenAlert(true));
+          dispatch(setMessage("Profile berhasil diperbarui!"));
+          dispatch(setSeverity("success"));
+          setTimeout(onClose, 2000);
+        }
+      })
+      .catch((err) => {
+        dispatch(setOpenAlert(true));
+        dispatch(setMessage(err?.response?.data?.message));
+        dispatch(setSeverity("error"));
+      });
+  };
 
   return (
     <FrameModal open={open} handleClose={onClose} title="Tambah Keahlian">
@@ -96,30 +115,56 @@ const AddSkill = ({ open, onClose}) => {
 
       <div>
         {selectedSkills && selectedSkills.length > 0 && (
-          <div style={{marginTop: 20, marginBottom: 20, display: 'flex', gap: '15px'}} >
+          <div
+            style={{
+              marginTop: 20,
+              marginBottom: 20,
+              display: "flex",
+              gap: "15px",
+            }}
+          >
             {selectedOptions.map((selectedValue, index) => {
               return (
-                <CustomChip key={index} sx={{bgcolor: '#FF9D3E'}} label={selectedValue.label} onDelete={() => handleRemoveSkill(selectedSkills)}/>
+                <CustomChip
+                  key={index}
+                  sx={{ bgcolor: "#FF9D3E" }}
+                  label={selectedValue.label}
+                  onDelete={() => handleRemoveSkill(index)}
+                />
               );
             })}
           </div>
         )}
       </div>
-      <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'flex-end'}} >
-        <button onClick={onClose} style={{maxWidth: 120, marginRight: 20}} className="btn  ">
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "flex-end",
+        }}
+      >
+        <button
+          onClick={onClose}
+          style={{ maxWidth: 120, marginRight: 20 }}
+          className="btn  "
+        >
           <span>Batal</span>
         </button>
-        <button style={{maxWidth: 120}} onClick={handleSaveSkill} className="btn btn-primary ">
+        <button
+          style={{ maxWidth: 120 }}
+          onClick={handleSaveSkill}
+          className="btn btn-primary "
+        >
           <span>Simpan</span>
         </button>
       </div>
       <CustomAlert
-            open={isOpenAlert} 
-            severity={alertSeverity}
-            text={alertMsg}
-            duration={2000} 
-            onClose={() => dispatch(setOpenAlert(false))} 
-        />
+        open={isOpenAlert}
+        severity={alertSeverity}
+        text={alertMsg}
+        duration={2000}
+        onClose={() => dispatch(setOpenAlert(false))}
+      />
     </FrameModal>
   );
 };
--- src/components/candidate/navbar.js
+++ src/components/candidate/navbar.js
@@ -1,81 +1,102 @@
-import React from 'react'
-import cn from 'classnames'
-import styles from '@/styles/components/candidate/Navbar.module.scss'
-import { Avatar, Menu, MenuItem, } from '@mui/material'
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import { useSelector } from 'react-redux'
-import { selectUser } from 'src/redux/common/userSlice'
+import React from "react";
+import cn from "classnames";
+import styles from "@/styles/components/candidate/Navbar.module.scss";
+import { Avatar, Menu, MenuItem } from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+import { selectUser } from "src/redux/common/userSlice";
+import { removeToken } from "src/utils/token";
 
 export default function Navbar() {
-    const [openDropdown, setOpenDropdown] = React.useState(false)
-    const router = useRouter()
-    const user = useSelector(selectUser);
+  const [openDropdown, setOpenDropdown] = React.useState(false);
+  const router = useRouter();
+  const user = useSelector(selectUser);
 
-    const handleDropdown = () => {
-        setOpenDropdown(!openDropdown)
-    }
+  const goToProfile = () => {
+    router.push("/candidate/profile");
+  };
 
-    return (<>
-    <nav className={cn(styles.navWrapper, "navbar navbar-expand-lg")}>
+  const logout = () => {
+    router.push("/login");
+    removeToken()
+  }
+
+  const handleDropdown = () => {
+    setOpenDropdown(!openDropdown);
+  };
+
+  return (
+    <>
+      <nav className={cn(styles.navWrapper, "navbar navbar-expand-lg")}>
         <div className="container-fluid">
-            <div className={cn(styles.group, "collapse navbar-collapse")} id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                        <Link className="nav-link" aria-current="page" href="/candidate/home">Lowongan Kerja</Link>
-                    </li>
-                </ul>
-                <div className={styles.accountMenu}>
-                    <div onClick={handleDropdown} className={styles.userBtn}>
-                        <Avatar src={user?.img} sx={{ width: 32, height: 32 }} />
-                        <div>
-                            <span>{user?.full_name}</span>
-                            <ExpandMoreIcon sx={{ color: '#F5F5F5' }} />
-                        </div>
-                    </div>
-                    <Menu
-                        open={openDropdown}
-                        onClick={handleDropdown}
-                        sx={{
-                            '& .MuiPaper-root': {
-                                position: 'absolute',
-                                top: '40px !important',
-                                left: '1015px !important',
-                                width: '180px'
-                            }
-                        }}
-                        id="account-menu"
-                        PaperProps={{
-                            elevation: 0,
-                            sx: {
-                                overflow: 'visible',
-                                filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-                                mt: 1.5,
-                                '&:before': {
-                                    content: '""',
-                                    display: 'block',
-                                    position: 'absolute',
-                                    top: 0,
-                                    right: 14,
-                                    width: 10,
-                                    height: 10,
-                                    bgcolor: 'background.paper',
-                                    transform: 'translateY(-50%) rotate(45deg)',
-                                    zIndex: 0,
-                                },
-                            },
-                        }}
-                    >
-                        <MenuItem onClick={handleDropdown} className={styles.userOption}>
-                            <Link href='/candidate/profile'>Profil Saya</Link>
-                        </MenuItem>
-                        <MenuItem onClick={handleDropdown}>Ubah password</MenuItem>
-                        <MenuItem onClick={handleDropdown}>Logout</MenuItem>
-                    </Menu>
+          <div
+            className={cn(styles.group, "collapse navbar-collapse")}
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  aria-current="page"
+                  href="/candidate/home"
+                >
+                  Lowongan Kerja
+                </Link>
+              </li>
+            </ul>
+            <div className={styles.accountMenu}>
+              <div onClick={handleDropdown} className={styles.userBtn}>
+                <Avatar src={user?.img} sx={{ width: 32, height: 32 }} />
+                <div>
+                  <span>{user?.full_name}</span>
+                  <ExpandMoreIcon sx={{ color: "#F5F5F5" }} />
                 </div>
+              </div>
+              <Menu
+                open={openDropdown}
+                onClick={handleDropdown}
+                sx={{
+                  "& .MuiPaper-root": {
+                    position: "absolute",
+                    top: "40px !important",
+                    left: "1015px !important",
+                    width: "180px",
+                  },
+                }}
+                id="account-menu"
+                PaperProps={{
+                  elevation: 0,
+                  sx: {
+                    overflow: "visible",
+                    filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                    mt: 1.5,
+                    "&:before": {
+                      content: '""',
+                      display: "block",
+                      position: "absolute",
+                      top: 0,
+                      right: 14,
+                      width: 10,
+                      height: 10,
+                      bgcolor: "background.paper",
+                      transform: "translateY(-50%) rotate(45deg)",
+                      zIndex: 0,
+                    },
+                  },
+                }}
+              >
+                <MenuItem onClick={goToProfile} className={styles.userOption}>
+                  Profil Saya
+                </MenuItem>
+                <MenuItem onClick={handleDropdown}>Ubah password</MenuItem>
+                <MenuItem onClick={logout}>Logout</MenuItem>
+              </Menu>
             </div>
+          </div>
         </div>
-    </nav>
-    </>)
+      </nav>
+    </>
+  );
 }
