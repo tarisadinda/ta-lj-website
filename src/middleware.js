72a77539be37c73c94812f375b18fa3ad2df2fd8
tarisadinda20 <tarisadinda20@gmail.com>
2023-11-30 10:46:19 +0700

    edit link page

--- src/middleware.js
+++ src/middleware.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 
 export function middleware(request) {
     if(!request.cookies.get("access_token")) {
-        return NextResponse.redirect(new URL('/login', request.url))
+        return NextResponse.redirect(new URL('/', request.url))
     }
 }
 
