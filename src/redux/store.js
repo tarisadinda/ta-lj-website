3827ac6b98e89d0a8a907ea2d97b64bfd2bc9c34
tarisadinda20 <tarisadinda20@gmail.com>
2023-12-09 18:32:33 +0700

    add redux thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,12 +3,16 @@ import alertSlice from "./common/alertSlice"
 import modalSlice from "./common/modalSlice"
 import userSlice from "./common/userSlice"
 import companySlice from "./common/companySlice"
+import careerLevelSlice from "./admin/careerLevelSlice"
+import { thunk } from "redux-thunk"
 
 export const store = configureStore({
     reducer: {
         alert: alertSlice,
         modal: modalSlice,
         user: userSlice,
-        company: companySlice
-    }
+        company: companySlice,
+        level: careerLevelSlice
+    },
+    middleware: [thunk]
 })
