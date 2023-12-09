56e75ba2a3c8d10182dca186c4746cff7035d76f
tarisadinda20 <tarisadinda20@gmail.com>
2023-12-10 00:31:43 +0700

    edit state name

--- src/pages/admin/company/detail-verification/[username].js
+++ src/pages/admin/company/detail-verification/[username].js
@@ -1,13 +1,13 @@
 import LayoutMain from "@/components/admin/layouts/main"
 import cn from 'classnames'
 import styles from '@/styles/pages/admin/company/DetailVerification.module.scss'
-import { Card } from '@mui/material'
+import { Avatar, Card } from '@mui/material'
 import PlaceIcon from '@mui/icons-material/Place'
 import Laptop from '@/public/laptop-work.png'
 import Image from "next/image"
 import Link from "next/link"
 import { axiosInstance } from "src/utils/axios"
-import { API_UNVERIF_COMPANY } from "src/utils/api"
+import { API_VERIF_COMPANY } from "src/utils/api"
 import React, { useEffect } from "react"
 import { useRouter } from "next/router"
 import { formatJsDate } from "src/utils/date-formatter"
@@ -18,7 +18,7 @@ export default function DetailVerification() {
     const pathUser = router.query.username
     console.log(router.query.username)
     const getDetail = () => {
-        axiosInstance.get(API_UNVERIF_COMPANY + "/" + pathUser)
+        axiosInstance.get(API_VERIF_COMPANY + "/" + pathUser)
         .then((res) => {
             console.log(res)
             setData(res.data.data)
@@ -34,7 +34,15 @@ export default function DetailVerification() {
     return(<>
         <h3><b>Permintaan Verifikasi</b></h3>
         <Card className={styles.infoWrapper} variant="outlined">
-            <Image src={`http://localhost/images/${data?.img}`} className={styles.logoCompany} width={150} height={150} alt="company-logo" />
+            {data?.img.includes('/null') ? 
+                <Avatar sx={{ width: 80, height: 80 }} /> :
+                <Image src={`http://localhost/images/${data?.img}`} 
+                    className={styles.logoCompany} 
+                    width={150} 
+                    height={150} 
+                    alt="company-logo" 
+                />
+            }
             <div className="d-flex flex-column justify-content-around">
                 <div>
                     <p className={styles.companyName}>{data?.full_name}</p>
@@ -59,10 +67,6 @@ export default function DetailVerification() {
                 <div className="row">
                     <p className="col-2"><b>Nomor Telepon</b></p>
                     <p className="col-6">{data?.company_detail.phone_number}</p>
-                </div>
-                <div className="row">
-                    <p className="col-2"><b>Alamat</b></p>
-                    <p className="col-6">{data?.company_detail.address}</p>
                 </div>
             </div>
         </div>
