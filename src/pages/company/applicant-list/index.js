edfeab1a292859ecb3b252e521ff8f6a20f5d2cd
tarisadinda20 <tarisadinda20@gmail.com>
2023-12-11 00:17:32 +0700

    integration

--- src/pages/company/applicant-list/index.js
+++ src/pages/company/applicant-list/index.js
@@ -3,59 +3,55 @@ import LayoutMain from "@/components/company/layouts/main"
 import CustomTable from "@/components/common/table"
 import { useRouter } from "next/router"
 import ConfirmDeleteModal from "@/components/common/confirm-delete"
-import React from "react"
+import React, { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { candidateAppliedList, fetchCandidateApplied } from "src/redux/company/candidateSlice"
+import { convertDate } from "src/utils/convert-date"
 
 const colNames = [
     {
-        id: 'name',
+        id: 'full_name',
         label: 'Nama Pelamar',
-        render: (data) => <span>{data.name}</span>
+        render: (data) => <span>{data.full_name}</span>
     },
     {
-        id: 'position',
+        id: 'job_name',
         label: 'Posisi',
-        render: (data) => <span>{data.position}</span>
+        render: (data) => <span>{data.job_name}</span>
     },
     {
-        id: 'skill',
-        label: 'Keahlian',
-        render: (data) => <span>{data.skill}</span>
-    },
-    {
-        id: 'date',
+        id: 'createdAt',
         label: 'Tgl Melamar',
-        render: (data) => <span>{data.date}</span>
+        render: (data) => <span>{convertDate(data.createdAt)}</span>
     },
     {
         id: 'status',
         label: 'Status',
-        render: (data) => <span>{data.status}</span>
-    },
-]
-
-const dummyData = [
-    {
-        id: 1,
-        name: 'Carissa Febrianti',
-        position: 'Mobile Developer',
-        skill: 'Level 1',
-        date: '27/09/2022',
-        status: 'Dalam review'
-    },
-    {
-        id: 2,
-        name: 'Budi Pranowo',
-        position: 'Web Developer',
-        skill: 'Level 2',
-        date: '27/09/2022',
-        status: 'Dalam review'
+        render: (data) => <span>{data.status == 'processed' ? 'Dalam review' :
+            data.status == 'accepted' ? 'Diterima' : 'Ditolak'}</span>
     },
 ]
 
 export default function ApplicantList() {
     const router = useRouter()
+    const dispatch = useDispatch()
 
     const [isDeleteModal, setIsDeleteModal] = React.useState(false)
+    const appliedList = useSelector(candidateAppliedList)
+    const dataApplied = appliedList.candidateList.data?.data.map((item) => ({
+            id: item.id,
+            full_name: item.CandidateDetail.user.full_name,
+            createdAt: item.createdAt,
+            job_name: item.job.name,
+            status: item.status
+    }))
+    const [page, setPage] = React.useState(0)
+
+    console.log(appliedList)
+    console.log(dataApplied)
+    const getCurrPage = (number) => {
+        setPage(number)
+    }
 
     const detailData = (id) => {
         router.push('/company/applicant-list/detail-applicant')
@@ -70,7 +66,11 @@ export default function ApplicantList() {
         console.log('get api delete')
         setIsDeleteModal(false)
     }
-    
+
+    useEffect(() => {
+        dispatch(fetchCandidateApplied())
+    }, [])
+
     return(<>
         <div className="d-flex gap-3 mb-5">
             <CountingCard total='12' title='Pelamar baru untuk direview' />
@@ -80,8 +80,11 @@ export default function ApplicantList() {
         <div>
             <CustomTable 
                 columns={colNames}
-                data={dummyData}
+                data={dataApplied}
                 idKey='id'
+                rowsPerPage='10'
+                totalData={dataApplied?.length}
+                getPage={getCurrPage}
                 detailFunc={detailData}
                 deleteFunc={deleteData}
             />
--- src/pages/company/edit-company-profile.js
+++ src/pages/company/edit-company-profile.js
@@ -1,11 +1,61 @@
+import CustomAlert from "@/components/common/alert"
 import LayoutMain from "@/components/company/layouts/main"
 import styles from '@/styles/pages/company/EditCompanyProfile.module.scss'
 import { Avatar } from "@mui/material"
 import cn from 'classnames'
+import { getCookie } from "cookies-next"
 import { useRouter } from "next/router"
+import React from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { alertMessage, openAlert, setMessage, setOpenAlert, setSeverity, severity } from "src/redux/common/alertSlice"
+import { API_COMPANY_PROFILE } from "src/utils/api"
+import { axiosInstance } from "src/utils/axios"
+import { dataCompany } from "src/utils/data-cookies"
 
 export default function CompanyEditForm() {
     const router = useRouter()
+    const dispatch = useDispatch()
+
+    const imageRef = React.useRef(null)
+    const tempDataCompany = typeof window != undefined ? getCookie("company_detail") : null
+    const readDataCompany = tempDataCompany != null ? JSON.parse(tempDataCompany) : null
+
+    const isOpenAlert = useSelector(openAlert)
+    const alertMsg = useSelector(alertMessage)
+    const alertSeverity = useSelector(severity)
+
+    const [companyImg, setCompanyImg] = React.useState('')
+    const [imgPreview, setImgPreview] = React.useState('')
+    const [formData, setFormData] = React.useState({
+        full_name: readDataCompany != null ? readDataCompany.full_name : "",
+        email: readDataCompany != null ? readDataCompany.email : "",
+        phone_number: readDataCompany != null ? readDataCompany.company_detail.phone_number : "",
+        address: readDataCompany != null ? readDataCompany.company_detail.address : "",
+        about_company: readDataCompany != null ? readDataCompany.company_detail.about_company : ""
+    })
+    
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const selectImage = (e) => {
+        e.preventDefault()
+
+        imageRef.current.click()
+    }
+
+    const uploadImage = (e) => {
+        console.log(e.target.files[0])
+        setCompanyImg(e.target.files[0])
+        setImgPreview(URL?.createObjectURL(e.target.files[0]))
+    }
+
+    console.log(imgPreview)
+    console.log(companyImg)
+    console.log(readDataCompany)
 
     const cancelBtn = (e) => {
         e.preventDefault()
@@ -13,50 +63,136 @@ export default function CompanyEditForm() {
         router.back()
     }
     
+    const updateProfile = (e) => {
+        e.preventDefault()
+
+        let submitData
+
+        if(companyImg == "") {
+            submitData = {
+                address: formData.address,
+                phone_number: formData.phone_number,
+                logo_company: readDataCompany.img,
+                about_company: formData.about_company,
+            }
+        } else {
+            submitData = {
+                address: formData.address,
+                phone_number: formData.phone_number,
+                logo_company: companyImg,
+                about_company: formData.about_company
+            }
+        }
+        
+        axiosInstance.post(API_COMPANY_PROFILE, submitData)
+        .then((res) => {
+            console.log(res)
+            dispatch(setOpenAlert(true))
+            dispatch(setMessage('Data berhasil diperbarui!'))
+            dispatch(setSeverity('success'))
+
+            router.push('/company/company-profile')
+        }).catch((err) => {
+            console.log(err)
+            dispatch(setOpenAlert(true))
+            dispatch(setMessage(err.response.data.message))
+            dispatch(setSeverity('error'))
+        })
+    }
+
+    const removeImg = () => {
+        setCompanyImg('')
+        setImgPreview('')
+    }
+
     return(<>
         <div>
             <h3><b>Edit Profil Perusahaan</b></h3>
             <div className={styles.avaSection}>
-                <Avatar sx={{ width: 150, height: 150 }} />
+                <Avatar sx={{ width: 150, height: 150 }} src={imgPreview == "" ? readDataCompany?.img : imgPreview} />
                 <div className={styles.btnList}>
-                    <button className={cn(styles.actionBtn, "btn btn-primary blue")}>Ubah Foto Profil</button>
-                    <button className={cn(styles.actionBtn, "btn btn-secondary blue")}>Hapus Foto Profil</button>
+                    <input 
+                        type="file"
+                        style={{ display: 'none' }}
+                        ref={imageRef}
+                        accept="image/*"
+                        onChange={uploadImage}
+                    />
+                    <button onClick={selectImage} className={cn(styles.actionBtn, "btn btn-primary blue")}>Ubah Foto Profil</button>
+                    <button onClick={removeImg} className={cn(styles.actionBtn, "btn btn-secondary blue")}>Hapus Foto Profil</button>
                 </div>
             </div>
             <div className={styles.formSection}>
                 <form>
                     <div className={styles.inputGroup}>
                         <label>Nama Perusahaan</label>
-                        <input type="text" placeholder="Masukkan nama perusahaan" className="form-control" />
+                        <input 
+                            type="text" 
+                            placeholder="Masukkan nama perusahaan" 
+                            className="form-control" 
+                            name="full_name"
+                            value={formData.full_name}
+                            onChange={handleChange}
+                            disabled
+                        />
                     </div>
                     <div className={styles.inputGroup}>
                         <label>Email Perusahaan</label>
-                        <input type="email" placeholder="Masukkan email perusahaan" className="form-control" />
+                        <input 
+                            type="email" 
+                            placeholder="Masukkan email perusahaan" 
+                            className="form-control" 
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            disabled
+                        />
+                    </div>
+                    <div className={styles.inputGroup}>
+                        <label>Nomor Telepon</label>
+                        <input 
+                            type="text" 
+                            placeholder="Masukkan nomor telepon perusahaan" 
+                            className="form-control" 
+                            name="phone_number"
+                            value={formData.phone_number}
+                            onChange={handleChange}
+                        />
                     </div>
                     <div className={styles.inputGroup}>
                         <label>Alamat Perusahaan</label>
-                        <textarea placeholder="Masukkan alamat perusahaan" className="form-control" />
+                        <textarea 
+                            placeholder="Masukkan alamat perusahaan" 
+                            className="form-control" 
+                            name="address"
+                            value={formData.address}
+                            onChange={handleChange}
+                        />
                     </div>
                     <div className={styles.inputGroup}>
                         <label>Tentang Perusahaan</label>
-                        <textarea placeholder="Deskripsikan perusahaan anda" className="form-control" />
-                    </div>
-                    <div className={styles.inputGroup}>
-                        <label>Kategori Perusahaan</label>
-                        <select className="form-select">
-                            <option value="1">1</option>
-                            <option value="1">2</option>
-                            <option value="1">3</option>
-                            <option value="1">4</option>
-                        </select>
+                        <textarea 
+                            placeholder="Deskripsikan perusahaan anda" 
+                            className={cn(styles.about, "form-control")}
+                            name="about_company"
+                            value={formData.about_company}
+                            onChange={handleChange}
+                        />
                     </div>
                     <div className={styles.formBtn}>
                         <button onClick={cancelBtn} className="btn btn-secondary blue">Batal</button>
-                        <button className="btn btn-primary blue">Perbarui Profil</button>
+                        <button onClick={updateProfile} className="btn btn-primary blue">Perbarui Profil</button>
                     </div>
                 </form>
             </div>
         </div>
+        <CustomAlert 
+            open={isOpenAlert} 
+            severity={alertSeverity}
+            text={alertMsg}
+            duration={2800} 
+            onClose={() => dispatch(setOpenAlert(false))} 
+        />
     </>)
 }
 
